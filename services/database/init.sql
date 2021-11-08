88b517b970da4a6194a5bd3ffbe7ee68f20847ab
lumencode <lumencoder@gmail.com>
2021-11-08 15:22:16 +0500

    Create Database Model

---
+++ services/database/init.sql
@@ -0,0 +1,33 @@
+create table pathways (
+	pathway_id serial not null primary key,
+	name character varying(128) not null
+);
+
+create table courses (
+	course_id serial not null primary key,
+	name character varying(128) not null,
+	pathway_id int not null references pathways(pathway_id)
+);
+
+create table takeaways (
+	takeaway_id serial not null primary key,
+	content character varying(256) not null,
+	course_id int not null references courses(course_id)
+);
+
+create table modules (
+	module_id serial not null primary key,
+	name character varying(128) not null,
+	course_id int not null references courses(course_id)
+);
+
+create table lessons (
+	lesson_id serial not null primary key,
+	name character varying(128) not null,
+	module_id int not null references modules(module_id)
+);
+
+create table contents (
+	content_id serial not null primary key,
+	lesson_id int not null references lessons(lesson_id)
+);
