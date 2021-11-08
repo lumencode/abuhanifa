5c59c76714336aed71527e88e22c3162da864e6f
lumencode <lumencoder@gmail.com>
2021-11-08 20:27:44 +0500

    Create react app

--- services/database/mock.sql
+++ services/database/mock.sql
@@ -2,3 +2,6 @@ insert into pathways (name) values ('PathWay #1'), ('PathWay #2');
 
 insert into courses (name, pathway_id) values
 ('Course #1', 1), ('Course #2', 1), ('Course #3', 2);
+
+insert into chapters (name, course_id) values
+('Chapter A', 1), ('Chapter B', 1), ('Chapter #1', 2), ('Chapter #2', 2), ('Chapter #3', 2);
---
+++ services/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App () {
+
+	return (
+		<>
+			Imom Abu Hanifa Xalqaro Islom Akademiyasi
+		</>
+	)
+}
+
+export default App
